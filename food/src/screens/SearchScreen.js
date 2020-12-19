c605b5703a1f901ebd9e1dffa7b8e5f5ddacf894
jacjanowski <jjanow7@uic.edu>
2020-12-19 14:32:05 -0600

    making request via Axios

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../API/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
   
+  const searchAPI = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose'
+      }
+    });
+    setResults(response.data.businesses)
+  }
+
   return (
     <View>
-      <SearchBar term={term} 
-      onTermChange={newTerm => setTerm(newTerm)} 
-      onTermSubmit = {() => console.log('term was submitted')}
+      <SearchBar term={term}
+        onTermChange={setTerm}
+        onTermSubmit={searchAPI}
       />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>we have found {results.length}</Text>
     </View>
   );
 };
