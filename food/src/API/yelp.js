2ab0aa057f7409e7aa90e3857ec9818bd7374def
jacjanowski <jjanow7@uic.edu>
2020-12-19 14:58:07 -0600

    first initial search

--- food/src/API/yelp.js
+++ food/src/API/yelp.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer xDzgQlDvyFYNfwe-BbmQPpG4UIXUOocacocpiU0_jVz-KtiDqysFLUhQzZFYR3Y0Rqi5l4bNUEV0SmKcJmKA61NuIDDPdAC9dwAzBgns6nMOrILhU7TKu1dadyrdX3Yx'
+        Authorization: 
+        'Bearer xDzgQlDvyFYNfwe-BbmQPpG4UIXUOocacocpiU0_jVz-KtiDqysFLUhQzZFYR3Y0Rqi5l4bNUEV0SmKcJmKA61NuIDDPdAC9dwAzBgns6nMOrILhU7TKu1dadyrdX3Yx'
     }
 
 
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -6,25 +6,37 @@ import yelp from '../API/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
-  
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+
   const searchAPI = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses)
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses)
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
   }
+
+  //call search API when first rendered
+  // aka BAD CODE!
+  //---> searchAPI('pasta');
 
   return (
     <View>
       <SearchBar term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchAPI}
+        onTermSubmit={() => searchAPI(term)}
+
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>we have found {results.length}</Text>
     </View>
   );
