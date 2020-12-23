ef745fc0b3907e35208928dd86c59aeee9273838
jacjanowski <jjanow7@uic.edu>
2020-12-23 10:04:00 -0600

    finish app.

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import {View , Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState}from 'react';
+import {View , Text, StyleSheet, Flatlist, Image} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import yelp from '../API/yelp';
 
 const ResultsShowScreen = ({navigation}) => {
+    const [result, setResult] = useState();
     const id = navigation.getParam('id'); //gets the parameter with attribute 'id'
-    console.log(id);
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect( () => {
+        getResult(id);
+    }, []);
+    
+    if(!result) {
+        return null;
+    }
+
     return(
         <View>
-            <Text>Results Show</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+            data={result.photos}
+            keyExtractor={(photo)=> photo} 
+            renderItem={({item}) =>{
+                return <Image style={styles.image} source={{uri: item}} />
+            }} />
         </View>
     );
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300
+    }
+});
 
 export default ResultsShowScreen;
